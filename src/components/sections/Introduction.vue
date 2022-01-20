503df92b4ee7e76d7bc8610eaf32fae068ccfe79
ben2613 <sosgyuki@gmail.com>
2022-01-20 16:26:58 +0000

    wow animation

--- src/components/sections/Introduction.vue
+++ src/components/sections/Introduction.vue
@@ -9,7 +9,7 @@
       </div>
     </div>
     <div absolute right-1 bottom-0 flex content-end>
-      <img class="h-30% max-h-30%" src="@/assets/byebye_boy.png" alt="Greeting Boy">
+      <img animate-spin animate="count-infinite 2s" class="greeting-boy h-30% max-h-30%" src="@/assets/byebye_boy.png" alt="Greeting Boy">
     </div>
   </div>
 </template>
@@ -19,4 +19,13 @@
 </script>
 
 <style lang="scss" scoped>
+@keyframes greeting {
+  0% {transform:rotate(-10deg);}
+  50% {transform:rotate(0deg);}
+  100% {transform:rotate(10deg);}
+}
+.greeting-boy {
+  animation-name: greeting;
+  animation-direction: alternate;
+}
 </style>
