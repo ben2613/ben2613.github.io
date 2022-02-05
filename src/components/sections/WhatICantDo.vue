09b03b7b6dd1b3ece8ac742bb7b2d084967676d2
ben2613 <sosgyuki@gmail.com>
2022-02-05 22:06:16 +0000

    wording

--- src/components/sections/WhatICantDo.vue
+++ src/components/sections/WhatICantDo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>What I Can't Do</h2>
-    <p>As you can see I am not able to do visual design tasks.</p>
+    <p>As you see I can't do visual design tasks.</p>
     <img src="@/assets/kaidan_taihen_man.png" />
   </div>
 </template>
