391dd4f85f2b7d134641e3ff2e874bba7808feb9
ben2613 <sosgyuki@gmail.com>
2022-02-05 22:01:02 +0000

    [Section] Detect content visibility and do transition

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,12 +1,21 @@
 <script setup>
+import { ref } from 'vue'
+import { useElementVisibility } from '@vueuse/core'
+
 defineProps({
   display: String, // display in the menu
   multiPage: Boolean, // to let the script
 })
+const target = ref(null)
+const isVisible = useElementVisibility(target)
 </script>
 <template>
-  <section :class="multiPage ? 'multi-page' : 'h-100vh'">
-    <slot></slot>
+  <section class="relative" :class="[multiPage ? 'multi-page' : 'h-100vh']">
+    <!-- for detect if some part of this region is entered-->
+    <div ref="target" class="absolute top-25vh bottom-25vh"></div>
+    <div class="transition-opacity ease-in-out duration-1000" :class="isVisible ? 'opacity-100' : 'opacity-0'">
+      <slot></slot>
+    </div>
   </section>
 </template>
 <style lang="scss">
